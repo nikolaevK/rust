def3c707701cea70d5bd8fd5aef93ae238481ff5
Konstantin Nikolaev <github@nikolaev.us>
2024-04-02 17:50:11 -0700

    Init Project Through Cargo Run

---
+++ server/target/debug/deps/server-c521928bebacc967.d
@@ -0,0 +1,5 @@
+/Users/konstantinnikolaev/projects/rust/server/target/debug/deps/libserver-c521928bebacc967.rmeta: src/main.rs
+
+/Users/konstantinnikolaev/projects/rust/server/target/debug/deps/server-c521928bebacc967.d: src/main.rs
+
+src/main.rs:
---
+++ server/target/debug/deps/server-efa10cca0edc6cb9.d
@@ -0,0 +1,5 @@
+/Users/konstantinnikolaev/projects/rust/server/target/debug/deps/libserver-efa10cca0edc6cb9.rmeta: src/main.rs
+
+/Users/konstantinnikolaev/projects/rust/server/target/debug/deps/server-efa10cca0edc6cb9.d: src/main.rs
+
+src/main.rs:

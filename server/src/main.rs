f7e5d96ce0d0c2e91ed48d3d494641852834a0bd
Konstantin Nikolaev <github@nikolaev.us>
2024-04-02 18:06:35 -0700

    Handle Connection

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 fn main() {
     let listener = 
@@ -7,6 +9,18 @@ fn main() {
         for stream in listener.incoming() {
             let stream = stream.unwrap();
             
-            println!("Connection established!");
+            handle_connection(stream);
         }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    
+    println!(
+        "Request: {}",
+        String::from_utf8_lossy(&buffer[..])
+    )
+
 }

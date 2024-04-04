852aeec17c864d53d9eeb1dbfeac85f300471791
Konstantin Nikolaev <github@nikolaev.us>
2024-04-03 17:53:27 -0700

    Desktop App Divides Income

--- incomeapp/src/main.rs
+++ incomeapp/src/main.rs
@@ -1,13 +1,30 @@
+use std::result;
+
 slint::include_modules!();
+
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFITPER: f64 = 0.05;
+const OPEXPER: f64 = 0.10;
+
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
+    ui.on_divide_income({
         let ui_handle = ui.as_weak();
-        move || {
+        move |string| {
             let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+
+            let num:f64 = string.trim().parse().unwrap();
+            let tax:f64 = num * TAXPER;
+            let owner:f64 = num * OWNERPER;
+            let profit:f64 = num * PROFITPER;
+            let opex:f64 = num * OPEXPER;
+
+            let result: String = format!("Taxes: {:2}\nOwner: {:2}\nProfit: {:2}\nOpex: {:2}", {tax}, {owner}, {profit}, {opex});
+
+            ui.set_results(result.into());
         }
     });
 

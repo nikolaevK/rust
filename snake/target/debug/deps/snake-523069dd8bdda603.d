8c7542cc469ad3a66bd4b5321cd6e749a2a5fef7
Konstantin Nikolaev <github@nikolaev.us>
2024-04-12 15:59:27 -0700

    INIT Of Snake

---
+++ snake/target/debug/deps/snake-523069dd8bdda603.d
@@ -0,0 +1,5 @@
+/Users/konstantinnikolaev/projects/rust/snake/target/debug/deps/libsnake-523069dd8bdda603.rmeta: src/main.rs
+
+/Users/konstantinnikolaev/projects/rust/snake/target/debug/deps/snake-523069dd8bdda603.d: src/main.rs
+
+src/main.rs:

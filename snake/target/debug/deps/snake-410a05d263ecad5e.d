a9a616b1bf1538a57941335b578d5479015b0f35
Konstantin Nikolaev <github@nikolaev.us>
2024-04-12 19:22:44 -0700

    Delete snake/target/debug directory

--- snake/target/debug/deps/snake-410a05d263ecad5e.d
+++
@@ -1,5 +0,0 @@
-/Users/konstantinnikolaev/projects/rust/snake/target/debug/deps/libsnake-410a05d263ecad5e.rmeta: src/main.rs
-
-/Users/konstantinnikolaev/projects/rust/snake/target/debug/deps/snake-410a05d263ecad5e.d: src/main.rs
-
-src/main.rs:

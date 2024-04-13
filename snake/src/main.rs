539b616a12a74434a37ff1f13da1216cfcd745df
Konstantin Nikolaev <github@nikolaev.us>
2024-04-12 18:07:09 -0700

    Implement Crate snake & draw

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,7 +1,12 @@
+
 extern crate rand;
 extern crate piston_window;
 
 mod draw;
+mod snake;
+mod game;
+
 fn main() {
-    println!("Hello, world!");
+    let snake = snake::Snake::new(1,1);
+    println!("Snake order {:#?}", snake.body.front()); 
 }

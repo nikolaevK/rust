f2795232f109608a20235d0ba0435cb96ba557a0
Konstantin Nikolaev <github@nikolaev.us>
2024-06-04 15:19:04 -0700

    Iterator Flatten Count

--- iterators/src/flatten.rs
+++ iterators/src/flatten.rs
@@ -1,9 +1,9 @@
-pub fn flatten<I>(iter: I) -> Flatten<I>
+pub fn flatten<I>(iter: I) -> Flatten<I::IntoIter>
 where
-    I: Iterator,
+    I: IntoIterator,
     I::Item: IntoIterator,
 {
-    Flatten::new(iter)
+    Flatten::new(iter.into_iter())
 }
 
 pub struct Flatten<O>
@@ -66,6 +66,6 @@ mod tests {
     }
     #[test]
     fn two_wide() {
-        assert_eq!(vec![vec!["a"], vec!["b"]].into_iter().count(), 2);
+        assert_eq!(flatten(vec![vec!["a"], vec!["b"]]).count(), 2);
     }
 }

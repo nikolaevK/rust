af7dbf73ff7f47de452569e2643c95b8f8a2e069
Konstantin Nikolaev <github@nikolaev.us>
2024-06-19 22:28:25 -0700

    Refactor the Code and Create & Test Add Route

--- crud-server/src/main.rs
+++ crud-server/src/main.rs
@@ -1,63 +1,112 @@
 use std::env;
 use dotenvy::dotenv;
-use libsql::{Builder, Connection, };
-use actix_web::{get, web::{self, Json, ServiceConfig}, Result};
+use libsql::{params, Builder, Database, Value};
+use actix_cors::Cors;
+use actix_web::{get,post, http::header, middleware::Logger, web::{self, Json, ServiceConfig}, Result};
 use serde::{Deserialize, Serialize};
 use shuttle_actix_web::ShuttleActixWeb;
 
 
-pub struct AppState {
-    db: Connection
+#[derive(Serialize, Deserialize)]
+struct Item {
+    id: i64, 
+    todo: String,
 }
 
 #[get("/")]
-async fn retrieve(state: web::Data<AppState>) -> Result<Json<Vec<Item>>> {    
-    let mut results = state
-    .db
-    .query("SELECT * FROM todo", ())
-    .await
-    .unwrap();
+async fn retrieve() -> Result<Json<Vec<Item>>> {
+    let db = connection().await;
+    let conn = db.connect().unwrap();
+    let results = conn
+    .query("SELECT * FROM todos", ())
+    .await;
 
     let mut items = Vec::new();
-
-    while let Some(row) = results.next().await.unwrap() {
-        let item: Item = Item {
-            id: row.get(0).unwrap(),
-            content: row.get(1).unwrap(),
-        };
-        items.push(item);
-    }
-
+    match results {
+        Ok(mut rows) => {
+            while let Some(row) = rows.next().await.unwrap() {
+                let item: Item = Item {
+                    id: row.get(0).unwrap(),
+                    todo: row.get(1).unwrap(),
+                };
+                items.push(item);
+            }
+        },
+        Err(_) => {
+            println!("Error retrieving");
+            ()
+        }
+    }   
     Ok(Json(items))
+}
+
+#[post("/add")]
+async fn add_todo(body: Json<Item>) -> Result<Json<bool>> {
+    let db = connection().await;
+    let conn = db.connect().unwrap();
+
+    let todo = conn
+        .execute("INSERT into todos (todo) VALUES (?1)", [body.todo.clone()])
+        .await;
+
+    // TODO: create separate modules and crates
+    // write good response
+    match todo {
+        Ok(_) => Ok(Json(true)),
+        Err(_) => Ok(Json(false)),
+    }
+    
 }
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    dotenv().expect(".env file not found");
-    let url = env::var("TURSO_DATABASE_URL").expect("URL must be set");
-    let token = env::var("TURSO_AUTH_TOKEN").unwrap_or_default();
 
-    let db = Builder::new_remote(url, token).build().await.expect("Something with database");
+    let db = connection().await;
     let conn = db.connect().unwrap();
 
-   let state = web::Data::new(AppState {
-    db:conn
-   });
-
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todos(id INTEGER PRIMARY KEY AUTOINCREMENT, todo VARCHAR)",
+        (),
+    )
+    .await
+    .unwrap();
+   
     let config = move |cfg: &mut ServiceConfig| {
+        let cors = Cors::default()
+                 .allowed_origin("http://localhost:5173")
+                 .allowed_origin("http://localhost:5173/")
+                 .allowed_methods(vec!["GET", "POST"])
+                 .allowed_headers(vec![
+                     header::CONTENT_TYPE,
+                     header::AUTHORIZATION,
+                     header::ACCEPT,
+                 ])
+                 .supports_credentials();
         cfg.service(
             web::scope("")
             .service(retrieve)
-            .app_data(state)
+            .service(add_todo)
+            .wrap(cors)
+            .wrap(Logger::default())
         );
 
     };
 
     Ok(config.into())
 }
-#[derive(Serialize, Deserialize)]
-struct Item {
-    id: i64, 
-    content: String,
+
+async fn connection() -> Database {
+    dotenv().expect(".env file not found");
+
+    let url = env::var("TURSO_DATABASE_URL").expect("URL must be set");
+    let token = env::var("TURSO_AUTH_TOKEN").unwrap_or_default();
+
+    let db = Builder::new_remote(url, token)
+        .build()
+        .await
+        .unwrap();
+
+    db
 }
+
 
